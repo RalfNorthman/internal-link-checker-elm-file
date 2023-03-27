4f333e4f4802843d0167f0cd760dab11f0814d3c
Ralf Northman <ralf.northman@bredband.net>
2023-03-27 09:22:23 +0200

    Compiles, but a lot of .unwrap()-calls.

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,46 @@
+use regex::Regex;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use regex::RegexSet;
 
 fn main() {
     let filename = "file.elm";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    let mut functions_and_types = Vec::new();
+    // elm type and function names
+    let mut identifiers = Vec::new();
 
-    let set = RegexSet::new(&[
-        r"^(\w*) : ",         // elm function name
-        r"^type (\w*)",       // elm type name
-        r"^type alias (\w*)", // elm type alias name
-    ])  
+    // elm module exposing list element
+    let exposed_item = Regex::new(r"^\s+,|\(\s(\w+)").unwrap();
 
-    for line in reader.lines() {
+    let lines = reader.lines().skip(1).map(|li| li.unwrap());
 
+    for line in lines {
+        if let Some(capture) = exposed_item.captures(&line) {
+            identifiers.push(capture.get(1).unwrap().as_str().to_string());
+        } else {
+            break;
+        }
     }
 
-    let mut broken_links = Vec::new();
+    // elm docs internal bookmark link
+    let anchor_link = Regex::new(r"\[(\w+)\]\((\#\w+)\)").unwrap();
 
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
         let line = line.unwrap();
-        for link in line.matches(|c| !c.is_ascii_alphanumeric() && c != '_') {
-            if link.starts_with("#") {
-                let name = &link[1..];
-                if !functions_and_types.contains(&name.to_string()) {
-                    broken_links.push(link.to_string());
-                }
+        for capture in anchor_link.captures_iter(&line) {
+            let whole = capture.get(0).unwrap().as_str();
+            let name = capture.get(1).unwrap().as_str();
+            let link = capture.get(2).unwrap().as_str();
+            if &name != &link {
+                println!("{}: name and link different.", whole);
             }
-        }
-    }
-
-    if !broken_links.is_empty() {
-        println!("Broken links:");
-        for link in broken_links {
-            println!("{}", link);
+            if !(identifiers.iter().any(|s| &s == &link)) {
+                println!("The link '{}' does not correspond to an identifier.", link);
+            }
         }
     }
 }
